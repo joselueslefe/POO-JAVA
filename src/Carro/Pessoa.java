2186f3e653ba5591037121c5bb81a2a680e64d7d
José <joselueslefelima@gmail.com>
2025-06-20 12:30:12 -0300

    estudando mais um pouco Poo haha

---
+++ src/Carro/Pessoa.java
@@ -0,0 +1,8 @@
+package Carro;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    String email;
+    String profissao;
+}
--- src/Carro/Test.java
+++ src/Carro/Test.java
@@ -2,6 +2,28 @@ package Carro;
 
 public class Test {
     public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa();
+
+        p1.nome = "José Leslefe";
+        p1.idade = 21;
+        p1.profissao = "Engenheiro de Software";
+        p1.email = "joselueslefelima@gmail.com";
+
+        p2.nome = "FerKp";
+        p2.idade = 24;
+        p2.profissao = "Engenheira de Software";
+        p2.email = "ferkpr@gmail.com";
+
+        System.out.println("Seu nome é: " + p1.nome + "," + "sua idade é: " + p1.idade +","
+                + "sua profissão é: " + p1.profissao + "," + "seu email é: " + p1.email);
+
+        System.out.println("Seu nome é: " + p2.nome + "," + "sua idade é: " + p2.idade + ","
+                + "sua profissão é: " + p2.profissao + "," + "seu email é: " + p2.email);
+
+
+
+
     Carro carro1 = new Carro();
     Carro carro2 = new Carro();
 
